92d1bd81745cd9b17dbd8e9d854448d09534968d
Axel Basset <axel23.b@gmail.com>
2018-11-08 14:44:48 +0100

    added new class and finished Aufgabe3

---
+++ src/Aufgabe3_1810653162.java
@@ -0,0 +1,22 @@
+public class Aufgabe3_1810653162
+{
+    public static void main(String[] args)
+    {
+        //Deklaration und Zuweisung von zahlen
+        int zahl1 = 1, zahl2 = 2, zahl3 = 3;
+        double zahl4 = 4.5, zahl5 = 5.5, zahl6 = 6.5;
+
+        //addierung
+        System.out.println(zahl1 + zahl2);
+        //dividierung
+        System.out.println(zahl1/zahl3);
+        //Multiplizierung
+        System.out.println(zahl4*zahl2);
+        //der Rest der Division
+        System.out.println(zahl1%zahl2);
+        //Inkrementiere Zahl bevor diese ausgegeben wird
+        System.out.println(++zahl3);
+        //Dekrementiere Zahl bevor diese ausgegeben wird
+        System.out.println(--zahl2);
+    }
+}
