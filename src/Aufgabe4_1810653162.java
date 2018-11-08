658e559e0dc646ea079c73bcd6c997eeb673080f
Axel Basset <axel23.b@gmail.com>
2018-11-08 14:51:43 +0100

    add new class, finished Aufgabe 4

---
+++ src/Aufgabe4_1810653162.java
@@ -0,0 +1,38 @@
+public class Aufgabe4_1810653162
+{
+    public static void main(String[] args)
+    {
+        //Deklaration und Zuweisung meiner 2 zahlen
+        int zahl1 = 1, zahl2 = 2;
+
+        //  links kleiner als rechts
+        System.out.println(zahl1 < zahl2);
+
+        //  links kleiner als rechts
+        System.out.println(zahl2 < zahl1);
+
+        //  links größer als rechts
+        System.out.println(zahl1 > zahl2);
+
+        //  links größer als rechts
+        System.out.println(zahl2 > zahl1);
+
+        //  links kleiner gleich rechts
+        System.out.println(zahl1 <= zahl2);
+
+        //  links kleiner gleich rechts
+        System.out.println(zahl2 <= zahl1);
+
+        //  links größer gleich rechts
+        System.out.println(zahl1 >= zahl2);
+
+        //  links größer gleich rechts
+        System.out.println(zahl2 >= zahl1);
+
+        //  links gleich rechts
+        System.out.println(zahl1 == zahl2);
+
+        //  links ungleich als rechts
+        System.out.println(zahl1 != zahl2);
+    }
+}
